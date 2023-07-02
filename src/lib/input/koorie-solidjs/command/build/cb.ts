49775a62e5493b6784a9bd64f4b50b7246a3aa7e
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-07-02 13:25:43 +0000

    @koorie/solidjs v1.0.1
    switched to solidjs typescript template

--- src/lib/input/koorie-solidjs/command/build/cb.ts
+++ src/lib/input/koorie-solidjs/command/build/cb.ts
@@ -3,7 +3,7 @@ import { spawn } from 'node:child_process'
 export default async function cb( data: Input.ParsedArgv ):Promise<void>{
 
   // @ts-ignore: @to-fix
-  const conf: string = data.flag?.[ '--config' ] ?? `${process.cwd()}/node_modules/@koorie/solidjs/vite.config.js`
+  const conf: string = data.flag?.[ '--config' ] ?? `${process.cwd()}/node_modules/@koorie/solidjs/vite.config.ts`
 
   spawn( 'node_modules/.bin/vite', [ '--emptyOutDir', '--config', conf, 'build' ], {
     stdio: [ 'ignore', 'inherit', 'inherit' ]
