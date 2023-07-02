f4a9ea2baa26d7de9e21fee74c2bccb0c1b0bf81
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-07-02 13:37:18 +0000

    @koorie/solidjs v1.0.1
    switched to solidjs typescript template

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import solidPlugin from 'vite-plugin-solid'
-import devtools from 'solid-devtools/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig( {
-  plugins: [ solidPlugin(), devtools() ],
+  plugins: [ solidPlugin() ],
   root :'app',
   build: {
     target: 'esnext',
